87c452f9b84dcc1d8e280abca1b56265854d3b4b
Santiago Martín Agra <santiagomartinagra@gmail.com>
2020-05-15 22:00:45 +0200

    feat: keep the connection with Twitch PubSub alive

--- src/twitch.js
+++ src/twitch.js
@@ -1,45 +1,81 @@
 const WebSocket = require("ws");
 const hue = require("./hue");
 
-const ws = new WebSocket("wss://pubsub-edge.twitch.tv");
-
-ws.on("open", function open() {
-  console.log("connected");
+function ping(ws) {
+  console.log("INFO: ping...");
   ws.send(
     JSON.stringify({
-      type: "LISTEN",
-      nonce: "44h1k13746815ab1r2",
-      data: {
-        topics: [`channel-points-channel-v1.${process.env.TWITCH_CHANNEL_ID}`],
-        auth_token: process.env.TWITCH_TOKEN,
-      },
+      type: "PING",
     })
   );
-});
+}
 
-ws.on("close", function close() {
-  console.log("disconnected");
-});
+function connect() {
+  const pingInterval = 1000 * 60; //ms between PING's
+  const reconnectInterval = 1000 * 3; //ms to wait before reconnect
+  let pingHandle;
 
-ws.on("message", function incoming(data) {
-  const json = JSON.parse(data);
-  if (!json.data) {
-    return;
-  }
+  const ws = new WebSocket("wss://pubsub-edge.twitch.tv");
 
-  const message = JSON.parse(json.data.message);
+  ws.on("open", function open() {
+    console.log("INFO: Connected");
+    ws.send(
+      JSON.stringify({
+        type: "LISTEN",
+        nonce: "44h1k13746815ab1r2",
+        data: {
+          topics: [
+            `channel-points-channel-v1.${process.env.TWITCH_CHANNEL_ID}`,
+          ],
+          auth_token: process.env.TWITCH_TOKEN,
+        },
+      })
+    );
 
-  if (!message.data.redemption.reward) {
-    return;
-  }
+    ping(ws);
+    pingHandle = setInterval(() => ping(ws), pingInterval);
+  });
 
-  const reward = message.data.redemption.reward;
+  ws.on("close", function close() {
+    console.log("INFO: Disconnected");
+    clearInterval(pingHandle);
+    console.log("INFO: Reconnecting...");
+    setTimeout(connect, reconnectInterval);
+  });
 
-  if (reward.id === process.env.TWITCH_REWARD_ID) {
-    hue({ on: false });
+  ws.on("message", function message(data) {
+    const json = JSON.parse(data);
 
-    setTimeout(() => {
-      hue({ on: true });
-    }, 10 * 1000);
-  }
-});
+    if (json.type === "PONG") {
+      console.log("INFO: ...pong");
+    }
+
+    if (json.type == "RECONNECT") {
+      console.log("INFO: Reconnecting...");
+      setTimeout(connect, reconnectInterval);
+    }
+
+    if (!json.data) {
+      return;
+    }
+
+    const message = JSON.parse(json.data.message);
+
+    if (!message.data.redemption.reward) {
+      return;
+    }
+
+    const reward = message.data.redemption.reward;
+
+    if (reward.id === process.env.TWITCH_REWARD_ID) {
+      console.log("INFO: Reward redeem");
+      hue({ on: false });
+
+      setTimeout(() => {
+        hue({ on: true });
+      }, 10 * 1000);
+    }
+  });
+}
+
+connect();
