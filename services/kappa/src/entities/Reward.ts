ee6641f112237d1c8cc4be604d0e34e8b7497b5d
Santi <SantiMA10@users.noreply.github.com>
2020-10-25 19:34:25 +0100

    feat(kappa): use lul package (#26)

--- services/kappa/src/entities/Reward.ts
+++
@@ -1,6 +0,0 @@
-export interface Reward {
-  id: string;
-  name: string;
-  date: Date;
-  message: string;
-}
--- services/kappa/src/services/task/TaskService.ts
+++
@@ -1,7 +0,0 @@
-export interface TaskService<Payload> {
-  create(options: {
-    name: string;
-    path: string;
-    payload: Payload;
-  }): Promise<void>;
-}
