856501f38428216ead6eaa297614b94c941587e5
Santiago Martín Agra <santiagomartinagra@gmail.com>
2020-10-31 12:53:37 +0100

    feat(light): use new 'RewardRedeemer'

--- services/light/src/routes/twitch/rewards/post.ts
+++ services/light/src/routes/twitch/rewards/post.ts
@@ -1,8 +1,5 @@
 import { Request, ResponseToolkit, ServerRoute } from "@hapi/hapi";
-import { Light } from "@streamdevs/lights-lul";
-import { Reward } from "@streamdevs/lights-lul";
-import { ChangeLightColor } from "@streamdevs/lights-lul";
-import { TurnOffAndOnLights } from "@streamdevs/lights-lul";
+import { Reward, RewardRedeemer } from "@streamdevs/lights-lul";
 
 export const routes = (): ServerRoute[] => {
   return [
@@ -13,21 +10,7 @@ export const routes = (): ServerRoute[] => {
         const { reward } = request.payload as { reward: Reward };
         console.log(JSON.stringify(reward));
 
-        const lights: Light[] = (process.env.LIFX_LIGHTS || "")
-          .split(",")
-          .map((id) => ({ id, service: "LIFX" }));
-
-        switch (reward.id) {
-          case "3cc420c0-2198-4cd8-9a07-c68f489c50be":
-            await new TurnOffAndOnLights().perform({ lights });
-            break;
-          case "d6909487-7ad1-4a95-a423-169cf465fb6d":
-            await new ChangeLightColor().perform({
-              lights,
-              message: reward.message,
-            });
-            break;
-        }
+        await new RewardRedeemer().perform({ reward });
 
         return h.response().code(204);
       },
